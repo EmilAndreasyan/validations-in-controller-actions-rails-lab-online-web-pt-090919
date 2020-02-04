bfaad3e665124d87ffccfbacdf262abd879349a2
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-04 03:05:55 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    
-    if @post.valid?
-      @post.update(post_params)
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
+  @post.update(post_params)
+  if @post.valid?
+    redirect_to post_path(@path)
   end
 
   private
